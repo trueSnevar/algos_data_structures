31e149eca959b7b30932e7ac4637b139f30f1f11
Aleksey Knyazev <aknyazev87@gmail.com>
2022-08-13 13:26:26 +0700

    Added more readable solution for palindrome

--- sprint_1/F.py
+++ sprint_1/F.py
@@ -11,9 +11,23 @@ def is_palindrome(line: str) -> bool:
     - Output: True
 
     """
-    normalized = ''.join([char.lower() for char in line if char.isalpha()])
-    reversed_str = normalized[::-1]
-    return normalized == reversed_str
+    # normalized = ''.join([char.lower() for char in line if char.isalpha()])
+    # reversed_str = normalized[::-1]
+    # return normalized == reversed_str
+    left = 0
+    right = len(line) - 1
+    while left <= right:
+        if not line[left].isalpha():
+            left += 1
+        elif not line[right].isalpha():
+            right -= 1
+        else:
+            if line[left].lower() != line[right].lower():
+                return False
+            else:
+                left +=1
+                right -= 1
+    return True
 
 if __name__ == "__main__":
     print(is_palindrome(input().strip()))
