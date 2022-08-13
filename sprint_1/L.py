c9bb8673764d66de3a54fdbe6c36c23e2191bd40
Aleksey Knyazev <aknyazev87@gmail.com>
2022-08-13 16:19:34 +0700

    Fixed L task in the 1st sprint

--- sprint_1/L.py
+++ sprint_1/L.py
@@ -1,27 +1,27 @@
-from typing import List, Tuple
+from typing import Tuple
 
-def get_distance(street_length: int, street_map: List[int]) -> List[int]:
-    result = [0] * street_length
-    curr_dist = None
-    for house_idx in range(street_length):
-        if street_map[house_idx] == 0:
-            curr_dist = 0
-            for back_idx in range(house_idx, -1, -1):
-                if curr_dist <= result[back_idx]:
-                    result[back_idx] = curr_dist
-                    curr_dist += 1
-                else:
-                    break
-            curr_dist = 0
-        else:
-            curr_dist += 1
-            result[house_idx] = curr_dist
-    return result   
+def get_excessive_letter(shorter: str, longer: str) -> str:
+    """ Get an excessive letter among 
+    the two given strings.
 
-def read_input() -> Tuple[int, List[int]]:
-    street_length = int(input())
-    street_map = list(map(int, input().strip().split()))
-    return street_map
+    - Example input: abcd
+                     abcde
 
-street_map = read_input()
-print(" ".join(map(str, get_distance(street_length, street_map))))
+    - Output: e
+
+    """
+    shorter_count = 0
+    longer_count = 0
+    for char in shorter:
+        shorter_count += ord(char)
+    for ch in longer:
+        longer_count += ord(ch)
+    return chr(longer_count-shorter_count)
+
+def read_input() -> Tuple[str, str]:
+    shorter = input().strip()
+    longer = input().strip()
+    return shorter, longer
+
+shorter, longer = read_input()
+print(get_excessive_letter(shorter, longer))
