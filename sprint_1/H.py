f19aa352e53abb92b2f54db6a7b575d477f0373b
Aleksey Knyazev <aknyazev87@gmail.com>
2022-08-13 14:17:35 +0700

    Added more concise solutiong for H task

--- sprint_1/H.py
+++ sprint_1/H.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from typing import Tuple
 
 def get_sum(first_number: str, second_number: str) -> str:
@@ -31,6 +33,34 @@ def get_sum(first_number: str, second_number: str) -> str:
         res += carry
 
     return res[::-1]
+
+def get_sum_of_bins(first_number: str, second_number: str) -> str:
+    """ Sums up two binary nums, represented as strings.
+
+    - Example input: 1010
+                     1011
+
+    - Output: 10101
+    
+    """
+    if len(second_number) > len(first_number):
+        first_number, second_number = second_number, first_number
+    diff = len(first_number) - len(second_number)
+    second_number = '0' * diff + second_number
+
+    res = ''
+    carry = 0
+    for i in range(len(first_number)-1, -1, -1):
+        s = int(first_number[i]) + int(second_number[i]) + carry
+        if s > 1:
+            s -= 2
+            carry =1 
+        else:
+            carry = 0
+        res = str(s) + res
+    if carry:
+        res = '1' + res
+    return res
 
 def read_input() -> Tuple[str, str]:
     first_number = input().strip()
