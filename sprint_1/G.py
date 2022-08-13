eec153bd9f34ce71dac55f65488a57450c9c6f50
Aleksey Knyazev <aknyazev87@gmail.com>
2022-08-13 13:52:14 +0700

    Corrected G task

--- sprint_1/G.py
+++ sprint_1/G.py
@@ -1,27 +1,20 @@
-def get_sum(first_number: str, second_number: str) -> str:
+# -*- coding: utf-8 -*-
+
+def dec_to_binary(n: int) -> str:
+    """ Converts input decimal non negative 
+    integer into binary.
+
+    - Example input: 13
+    - Output: '1101'
+
+    """
     res = ''
-    carry = ''
-    for i in range(len(first_number)-1, -1, -1):
-        if (first_number[i] == '1' and second_number[i] == '1'):
-            res += '0'
-            if not carry:
-                carry += '1'
-        elif (first_number[i] == '0' and second_number[i] == '0'):
-            if not carry:
-                res += '0'
-            else:
-                res += '1'
-                carry = ''    
-        else:
-            if not carry:
-                res += '1'
-            else:
-                res += '0'
+    while n > 0:
+        bit = str(n % 2)
+        res = bit + res
+        n //= 2
+    return res
 
-    if carry:
-        res += carry
-
-    return res[::-1]
-
-print(get_sum('1010', '1011')) # 10101
-print(get_sum('1', '1')) # 10
+if __name__ == "__main__":
+    n = int(input())
+    print(dec_to_binary(n))
