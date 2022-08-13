9b0f4f9dfea08845f2624f1e53bd4da95dfe8244
Aleksey Knyazev <aknyazev87@gmail.com>
2022-08-13 13:10:53 +0700

    Added docstirng for palindrome func

--- sprint_1/F.py
+++ sprint_1/F.py
@@ -1,6 +1,19 @@
+# -*- coding: utf-8 -*-
+
 def is_palindrome(line: str) -> bool:
+    """ Checks is a given string
+    is a palindrome.
+
+    Consider alphanumeric symbols only.
+    Big and small chars treated the same.
+
+    - Example input: A man, a plan, a canal: Panama
+    - Output: True
+
+    """
     normalized = ''.join([char.lower() for char in line if char.isalpha()])
     reversed_str = normalized[::-1]
     return normalized == reversed_str
 
-print(is_palindrome(input().strip()))
+if __name__ == "__main__":
+    print(is_palindrome(input().strip()))
