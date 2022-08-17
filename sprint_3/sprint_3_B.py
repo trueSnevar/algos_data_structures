d7cab559ebd9e571bcdfd9aead3957a2dfca71b0
Aleksey Knyazev <aknyazev87@gmail.com>
2022-08-17 23:05:40 +0700

    added template for B tast, sprint 3

---
+++ sprint_3/sprint_3_B.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+MAPPING = {
+    2:'abc',
+    3:'def',
+    4:'ghi',
+    5:'jkl',
+    6:'mno',
+    7:'pqrs',
+    8:'tuv',
+    9:'wxyz' 
+}
+
+def make_combinations(code: str) -> List[str]:
+   # TODO
+   pass
