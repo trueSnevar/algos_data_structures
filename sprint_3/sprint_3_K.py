f48186026c60df4ebce75e9dcfcb65ecbffb7ddd
Aleksey Knyazev <aknyazev87@gmail.com>
2022-08-23 00:34:16 +0700

    finally done with merge sort

--- sprint_3/sprint_3_K.py
+++ sprint_3/sprint_3_K.py
@@ -1,8 +1,10 @@
 # coding: utf-8
+# https://stackoverflow.com/questions/66284003/problem-implementing-merge-sort-from-pseudo-code-python
+# https://contest.yandex.ru/contest/23638/problems/K/?success=69729386#51450/2020_12_13/PtZf82N8yq
 
 def merge(arr, lf, mid, rg):
 	left = arr[lf:mid]
-	right = arr[mid+1:rg]
+	right = arr[mid:rg]
 	idx = lf
 	l = 0
 	r = 0
@@ -26,10 +28,10 @@ def merge(arr, lf, mid, rg):
 	return arr
 
 def merge_sort(arr, lf, rg):
-	if lf < rg:
+	if rg > lf + 1:
 		mid = (lf + rg) // 2
 		merge_sort(arr, lf, mid)
-		merge_sort(arr, mid+1, rg)
+		merge_sort(arr, mid, rg)
 		merge(arr, lf, mid, rg)
 	
 
@@ -37,9 +39,9 @@ def test():
 	a = [1, 4, 9, 2, 10, 11]
 	b = merge(a, 0, 3, 6)
 	expected = [1, 2, 4, 9, 10, 11]
-	#assert b == expected
+	assert b == expected
 	c = [1, 4, 2, 10, 1, 2]
-	merge_sort(c, 0 , 5)
+	merge_sort(c, 0 , 6)
 	expected = [1, 1, 2, 2, 4, 10]
 	assert c == expected
 
