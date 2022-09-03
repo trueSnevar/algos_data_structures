07085760c32d3258d4c7a5d1d9401f3ff643da07
Aleksey Knyazev <aknyazev87@gmail.com>
2022-09-03 14:46:11 +0700

    implemented sprint 3, I solution

--- sprint_3/sprint_3_I.py
+++ sprint_3/sprint_3_I.py
@@ -1,11 +1,22 @@
 # coding: utf-8
+# https://contest.yandex.ru/contest/23638/problems/I/?success=69942044#2989/2020_04_13/QUOg7a2HDC
 
 from typing import List
 
 def conference_lovers(arr: List[int], k: int) -> List[int]:
-    arr.sorted()
-    max_val = arr[-1]
-    counter = [0] * max_val
+    m = {}
+    for el in arr:
+        if el in m:
+            m[el] += 1
+        else:
+            m[el] = 1
+    srt = [(k,v) for k,v in m.items()]
+    srt.sort(key=lambda x: (-x[1], x[0]))
+    res = [k for k,v in srt]   
+    return res[:k]
 
-    for item in arr:
-        max_val[item] += 1
+if __name__ == "__main__":
+    n = int(input())
+    lst = list(map(int, input().strip().split()))
+    k = int(input())
+    print(*conference_lovers(lst, k))
