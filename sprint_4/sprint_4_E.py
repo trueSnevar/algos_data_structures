83bdf136ae885c0c4ad23a30bdd7b65762aa1351
Aleksey Knyazev <aknyazev87@gmail.com>
2022-09-05 23:11:34 +0700

    added substring solution for E, sprint 4

--- sprint_4/sprint_4_E.py
+++ sprint_4/sprint_4_E.py
@@ -1,19 +1,18 @@
 # coding: utf-8
-# doesn't work
+# https://contest.yandex.ru/contest/23991/problems/E/?success=70017689#2989/2020_04_21/PHfYqqL8FT
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/
 
 
 def substring(s: str) -> int:
     m = {}
+    left = 0
     best = 0
-    curr = 0
-
-    for char in s:
-        if char not in m:
-            curr += 1
-            m[char] = True
+    for idx, char in enumerate(s):
+        if char in m and left <= m[char]:
+           left = m[char] + 1
         else:
-            best = max(best, curr)
-            curr = 0
+            best = max(best, idx - left + 1)
+        m[char] = idx
     return best
 
 if __name__ == "__main__":
